31d21f35de89aa40d47e034d35389fc8c1d28513
Pre05 <38923089+precooled05@users.noreply.github.com>
2019-05-15 05:59:34 +0100

    Update AutoUpdater.py

--- AutoUpdater.py
+++ AutoUpdater.py
@@ -1,4 +1,15 @@
 import subprocess
 
-subprocess.call("git pull", shell=True)
+DEBUG = False
+
+try:
+	subprocess.call("git pull", shell=True)
+	if DEBUG:
+		input()
+	else:
+		exit(0)
+
+except Exception as e:
+	print(f"ERROR (Fatal): {e}")
+	input("Press any button to exit...")
 
